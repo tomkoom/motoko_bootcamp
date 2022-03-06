15c04af1bb17c4c16bb3b9968b3c9d366ebd8e35
tomkoom <tomash.sugint@gmail.com>
2022-03-06 13:17:50 +0300

    feat: update folder structure

--- src/motoko_bootcamp_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { motoko_bootcamp } from "../../declarations/motoko_bootcamp";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with motoko_bootcamp actor, calling the greet method
-  const greeting = await motoko_bootcamp.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
